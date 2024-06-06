6630356dbf9539d77a4b794bef1f8814129bc07a
Alex <alexander.siracusa@gmail.com>
2024-06-06 16:23:44 -0400

    postgres api cleanup

--- backend/postgres_api/views/query.py
+++ backend/postgres_api/views/query.py
@@ -5,11 +5,11 @@ from django.db import connection
 
 
 def dictfetchall(cursor):
-    "Returns all rows from a cursor as a dict"
+    # Returns all rows from a cursor as a dict
     desc = cursor.description
     return [
-            dict(zip([col[0] for col in desc], row))
-            for row in cursor.fetchall()
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
     ]
 
 
