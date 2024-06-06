417e8754a248eb14de1a57907f534ca5a9eef387
HannaTrinh <trinhmaianhacg@gmail.com>
2024-06-06 14:34:53 -0400

    changed the routes in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
 import Home from './components/Home';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
