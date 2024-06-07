eb9737d93490e8717d52943231733f1cac293b29
golafsson <ggolafsson@wpi.edu>
2024-06-07 11:18:40 -0400

    added buttons on the landing page that direct to home and document respectively.

---
+++ frontend/src/components/Document.tsx
@@ -0,0 +1,9 @@
+const Document = () => {
+    return (
+        <div>
+            <h1 className="text-red-500">This is the document page!</h1>
+        </div>
+    );
+}
+
+export default Document;
