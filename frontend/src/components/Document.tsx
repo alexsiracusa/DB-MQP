b41588c08fc2f8902afe03f319c1f2291c4c5e1a
golafsson <ggolafsson@wpi.edu>
2024-06-07 13:47:11 -0400

    got the documment page added and did very minimal sytling. does not look like the figma at all yet

--- frontend/src/components/Document.tsx
+++ frontend/src/components/Document.tsx
@@ -1,7 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/Document.css";
+import "../styles/Global.css";
+
 const Document = () => {
     return (
-        <div>
-            <h1 className="text-red-500">This is the document page!</h1>
+        <div className={"full-screen"}>
+            <div className={"justify-content-center"}>
+                <h1>This is the document page!</h1>
+            </div>
+            <div className={"mt-5"}>
+                <p>This is some content in the lower part of the page.</p>
+                <p>It spans the entirety of the webpage.</p>
+
+            </div>
         </div>
     );
 }
