5896595d8f78c5db65d096c8ec01701bebb24070
golafsson <ggolafsson@wpi.edu>
2024-06-14 10:13:54 -0400

    idk what im commiting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import TopNavBar from "./components/TopNavBar";
 import Translator from './components/Translator';
 import LandingPage from './components/LandingPage';
 import Document from "./components/Document";
@@ -9,6 +10,7 @@ function App() {
   return (
       <>
       <Navbar />
+        <TopNavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/translator" element={<Translator />} />
