dd5446d3b2cf4d210b38c8b859bd17cbc4c6fedd
golafsson <ggolafsson@wpi.edu>
2024-06-24 14:18:41 -0400

    did the styling of the text and the buttons

--- frontend/src/pages/HomePageSections/QueriesSection.tsx
+++ frontend/src/pages/HomePageSections/QueriesSection.tsx
@@ -6,7 +6,14 @@ const QueriesSection: React.FC = () => {
     return (
         <div className="queries">
             <div className="content">
-                <h2>Queries Section</h2>
+                <div className="top-text">
+                    <h2>Explore and Develop Queries</h2>
+                    <p>Using our IDE you can write long sentences that fill space and look cool if your a big enough nerd for this kinda stuff. <br /> Bet you think this is cool. Nerd.</p>
+                </div>
+                <div className="buttons">
+                        <button className="translate">translate</button>
+                        <button className="documentation">documentation</button>
+                </div>
             </div>
         </div>
     );
