15a616c36368cde698ba9ade9730e320efc93bab
jlamenha <jlamenhamiranda@wpi.edu>
2024-06-28 11:40:37 -0400

    report section fully implemented with image linking working

--- frontend/src/components/ReportSection.tsx
+++ frontend/src/components/ReportSection.tsx
@@ -6,16 +6,24 @@ interface ReportSectionInt {
     altText: string;
     title: string;
     text: string;
+    link: string;
 }
 
-const Report: React.FC<ReportSectionInt> = ({ altText, title, text }) => {
+const Report: React.FC<ReportSectionInt> = ({ altText, title, text, link }) => {
+
+    const handleClick = () => {
+        window.open(link, '_blank');
+    };
+
     return(
+   <div className="report">     
     <div className="container">
-        <img src={ReportPageCover} alt={altText} className="image" />
-        <div>
+        <img src={ReportPageCover} alt={altText} className="image" onClick={handleClick} style={{ cursor: 'pointer' }}/>
+        <div className="text-content" >
         <h2 className="title">{title}</h2>
         <p className="text">{text}</p> 
         </div>
+    </div>
     </div>
     )
 }
