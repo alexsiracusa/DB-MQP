f5ce88204c96a5b2c6dae76db71866f4dc96f2e2
Alex <alexander.siracusa@gmail.com>
2024-07-05 21:21:10 -0400

    code editor setup

--- frontend/src/components/Tabs/TabContent/QueryTab/QueryTab.tsx
+++ frontend/src/components/Tabs/TabContent/QueryTab/QueryTab.tsx
@@ -6,6 +6,7 @@ type FileType = "pgSQL" | "MongoDB";
 class QueryTab extends TabContent {
     fileType: FileType = "pgSQL"
     locked: boolean = false;
+    query: string = "--SQL Query"
 
     constructor(name: string, parent: TabWindow) {
         super(name, parent);
--- frontend/src/components/Tabs/TabContent/QueryTab/QueryTabCode.tsx
+++ frontend/src/components/Tabs/TabContent/QueryTab/QueryTabCode.tsx
@@ -1,6 +1,7 @@
 import '../../../../styles/TabContent.css'
 import '../../../../styles/QueryTabCode.css'
 
+import Editor from '@monaco-editor/react';
 import QueryTab from "./QueryTab.tsx";
 
 type QueryTabCodeProps = {
@@ -10,9 +11,24 @@ type QueryTabCodeProps = {
 const QueryTabCode = (props: QueryTabCodeProps) => {
     const self = props.self;
 
+     // @ts-expect-error: doesn't like event not being used
+    function handleEditorChange(value: string | undefined, event: any) { // eslint-disable-line
+        self.query = value ? value : "";
+    }
+
     return (
         <div className="tab-content">
-            {self.id}
+            <Editor
+                height="100%"
+                defaultLanguage="sql"
+                defaultValue={self.query}
+                onChange={handleEditorChange}
+                options={{
+                    minimap: {
+                        enabled: false
+                    }
+                }}
+            />
         </div>
     )
 }
