e7866c81df38af5f3de314e013ab3055cded34c6
jlamenha <jlamenhamiranda@wpi.edu>
2024-07-17 11:55:30 -0400

    reworked navbar icon and font color

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -17,9 +17,7 @@ const Navbar: React.FC = () => {
           xmlns="http://www.w3.org/2000/svg"
           className="fill-current text-white h-8 w-8 mr-2"
         >
-          <path
-            d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z"
-          />
+          <image href="\src\assets\Icons\NavbarStack.png" x="0" y="0" height="24" width="24" />
         </svg>
         <span className="text-xl text-white font-bold uppercase tracking-wide" onClick={() => handleNavigation('/')}
           >Relational Remodel MQP</span>
@@ -39,20 +37,20 @@ const Navbar: React.FC = () => {
         >
           <a
             href="#"
-            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 hover:text-white"
           >
             
             <span onClick={() => handleNavigation('/Translator')}>Translator</span>
           </a>
           <a
             href="#"
-            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 hover:text-white"
           >
             <span>Documentation</span>
           </a>
           <a
             href="#"
-            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 hover:text-white"
           >
             <span>Contact Us</span>
           </a>
