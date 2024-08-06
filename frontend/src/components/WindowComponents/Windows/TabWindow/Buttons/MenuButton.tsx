9178ead953be547a1d4abb751089a318cd3833b0
Alex <alexander.siracusa@gmail.com>
2024-08-05 23:39:26 -0400

    renamed add pane buttons

--- frontend/src/components/WindowComponents/Windows/TabWindow/Buttons/MenuButton.tsx
+++ frontend/src/components/WindowComponents/Windows/TabWindow/Buttons/MenuButton.tsx
@@ -55,22 +55,22 @@ const MenuButton = (props: TabDropdownProps) => {
             >
                 <DropDownRow value={"pane-right"} className="menu-row">
                     <img src={PaneRight}/>
-                    Insert Right
+                    Pane Right
                 </DropDownRow>
 
                 <DropDownRow value={"pane-left"} className="menu-row">
                     <img src={PaneLeft}/>
-                    Insert Left
+                    Pane Left
                 </DropDownRow>
 
                 <DropDownRow value={"pane-up"} className="menu-row">
                     <img src={PaneUp}/>
-                    Insert Up
+                    Pane Up
                 </DropDownRow>
 
                 <DropDownRow value={"pane-down"} className="menu-row">
                     <img src={PaneDown}/>
-                    Insert Down
+                    Pane Down
                 </DropDownRow>
 
                 <DropDownRow value={"delete"} className="menu-row">
