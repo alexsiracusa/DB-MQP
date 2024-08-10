032a767d9b416e9359ba9437d026ded93ebf441b
jlamenha <jlamenhamiranda@wpi.edu>
2024-08-10 14:42:38 -0400

    added content to Report Section

--- frontend/src/components/ReportSection.tsx
+++ frontend/src/components/ReportSection.tsx
@@ -6,12 +6,12 @@ import ReportPageCover from '../assets/ReportSectionAssets/ReportCoverPage.png'
 
 interface ReportSectionInt {
     altText: string;
-    title: string;
-    text: string;
     link: string;
 }
 
-const Report: React.FC<ReportSectionInt> = ({altText, title, text, link}) => {
+                    
+
+const Report: React.FC<ReportSectionInt> = ({altText, link}) => {
 
     const handleClick = () => {
         window.open(link, '_blank');
@@ -22,8 +22,14 @@ const Report: React.FC<ReportSectionInt> = ({altText, title, text, link}) => {
             <img src={ReportPageCover} alt={altText} className="image" onClick={handleClick}
                  style={{cursor: 'pointer'}}/>
             <div className="text-content">
-                <h2 className="title">{title}</h2>
-                <p className="text">{text}</p>
+                <h2 className="title">Link to Report</h2>
+                <p className="text">This team's report on the project developed can be read by clicking the image. 
+                This report was written as part of a project graduation requirement at Worcester Polytechnic Institute. 
+                </p>
+                <br></br>
+                <h3 className="subtitle"> This is our abstract:</h3>
+                <br></br>
+                <p className="text">Add abstract here</p>
             </div>
         </div>
     )
